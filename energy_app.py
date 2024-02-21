19cff8a4135719e61b457afa94b7f900d2bcbc37
= <rswmallett@gmail.com>
2024-02-21 08:54:14 +0000

    first commit

--- energy_app.py
+++ energy_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 #https://tarjomefa.com/wp-content/uploads/2017/04/6453-English-TarjomeFa.pdf
-
-st.title("Heating / cooling predictor")
+#st.image("image.jpg")
+st.title("Heating / cooling load predictor")
 
 st.write("This app predicts the heating and cooling loads given eight input features.")
 
@@ -35,8 +35,10 @@ X6 = st.number_input("Orientation", 2.0, 5.0)
 X7 = st.number_input("Glazing area", 0.0, 0.4)
 X8 = st.number_input("Glazing area distribution", 0.0, 5.0)
 
-hpred = heating_model(X1,X2,X3,X4,X5,X6,X7,X8)
-cpred = cooling_model(X1,X2,X3,X4,X5,X6,X7,X8)
 
-st.write(f"The heating load prediction is: {hpred}")
-st.write(f"The cooling load prediction is: {cpred}")
+if st.button("Predict"):
+    hpred = heating_model(X1,X2,X3,X4,X5,X6,X7,X8)
+    cpred = cooling_model(X1,X2,X3,X4,X5,X6,X7,X8)
+
+    st.write(f"The heating load prediction is: {hpred}")
+    st.write(f"The cooling load prediction is: {cpred}")
